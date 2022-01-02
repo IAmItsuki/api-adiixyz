f7324b8b14504655bea6a9cee13439755dba81b6
buf = await downloadM() <82200440+Adiixyz@users.noreply.github.com>
2022-01-02 17:22:30 +0800

    Create api.js

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ let zahirr = db.get("zahirr");
 } catch (e) {
 	console.log('')  
 }
-let creator = "Velgrynd"
+let creator = "Adii"
 let axios = require('axios')
 let fs = require('fs')
 let fetch = require('node-fetch');
